38d17bbbc636dae0eb42cc6413b91308f836a764
Zetison <jonvegard89@yahoo.com>
2020-08-05 08:42:57 +0200

    updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # colormaps
 A collection of colormaps
+
+For the NCL color maps do the following
+```console
+export NCARG_COLORMAPS=$HOME/kode/colormaps:$NCARG_ROOT/lib/ncarg/colormaps
+python3 convert_json.py --inputname SINTEF1.json --outputname SINTEF1.rgb && cat *.rgb
+```
--- convert_json.py
+++ convert_json.py
@@ -4,14 +4,18 @@ import re
 import copy
 import scipy.interpolate as interpolate
 import numpy as np
+import os
 
 @click.command()
 @click.option('--inputname', type=str, required=True)
 @click.option('--outputname', type=str, required=False)
-def main(inputname,outputname):
+@click.option('--nrgbpts', default=1024)
+def main(inputname,outputname,nrgbpts):
 	f = open(inputname,'r')
 	if not(outputname):
 		outputname = inputname[:-4]+'xml'
+	filename, file_extension = os.path.splitext(outputname)
+
 	fout = open(outputname, 'w')
 	foundName = False
 	extractPoints = False
@@ -24,7 +28,8 @@ def main(inputname,outputname):
 		elif "Name" in x:
 			temp = re.findall('"([^"]*)"', x)
 			scheme = temp[1]
-			fout.write('<ColorMap name="%s" space="%s">\n' % (scheme, space) );
+			if file_extension == '.xml':
+				fout.write('<ColorMap name="%s" space="%s">\n' % (scheme, space) );
 			foundName = True
 		if '[' in x and foundName:
 			extractPoints = True
@@ -42,15 +47,30 @@ def main(inputname,outputname):
 			block += 1
 			data=[]
 	f.close()
-	spl = interpolate.UnivariateSpline(points, values, k=1, s=0)
 	noPoints = len(RGBpoints)//4
-	for i in range(0,noPoints):
-		x = RGBpoints[4*i]
-		o = spl(x)
-		fout.write('\t<Point x="%.15f" o="%.15f" r="%.15f" g="%.15f" b="%.15f"/>\n' % (x, o, RGBpoints[4*i+1], RGBpoints[4*i+2], RGBpoints[4*i+3]) );
+	if file_extension == '.rgb':
+		RGBpoints = np.reshape(np.array(RGBpoints),(noPoints,4))
+		temp = np.zeros((nrgbpts,3))
+		for i in range(1,4):
+			spl1 = interpolate.UnivariateSpline(RGBpoints[:,0], RGBpoints[:,i], k=1, s=0)
+			temp[:,i-1] = spl1(np.linspace(RGBpoints[0,0],RGBpoints[-1,0],nrgbpts))
+		RGBpoints = 255*temp
+		noPoints = nrgbpts
+		fout.write('ncolors=%d\n#  r   g   b\n' % (noPoints) )
+	elif file_extension == '.xml':
+		spl = interpolate.UnivariateSpline(points, values, k=1, s=0)
 
-	fout.write('</ColorMap>')
-	fout.close()
+	for i in range(0,noPoints):
+		if file_extension == '.xml':
+			x = RGBpoints[4*i]
+			o = spl(x)
+			fout.write('\t<Point x="%.15f" o="%.15f" r="%.15f" g="%.15f" b="%.15f"/>\n' % (x, o, RGBpoints[4*i+1], RGBpoints[4*i+2], RGBpoints[4*i+3]) );
+		elif file_extension == '.rgb':
+			fout.write('%d %d %d\n' % (RGBpoints[i,0], RGBpoints[i,1], RGBpoints[i,2]) );
+
+	if file_extension == '.xml':
+		fout.write('</ColorMap>')
+		fout.close()
 	
 
 if __name__ == '__main__':
